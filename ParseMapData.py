6c9251b998c6b8ef58be32b4686311f05171232e
Isaac Lau <ilauclee@outlook.com>
2018-06-14 23:53:19 -0400

    Use lists
    Added a few lists to capture vaious components' values

--- ParseMapData.py
+++ ParseMapData.py
@@ -7,6 +7,7 @@ Created on Fri Jun  8 21:38:11 2018
 
 #from bs4 import BeautifulSoup
 import bs4 as bs
+import matplotlib.pyplot as plt
 
 soup = None
 with open('Paris.xml') as sauce:
@@ -14,25 +15,27 @@ with open('Paris.xml') as sauce:
     sauce.close()
 
 i=0
+coords = []
+listLongitudes = []
+listLatitudes  = []
+listPOInames = []
+
 for placemark in soup.findAll ('Placemark'):
     i+=1
     fLong, fLat, _ = placemark.find('coordinates').text.split(',')
     fLong = float(fLong)
     fLat = float (fLat)
+    
+    POIname = placemark.find('name').text
+    listPOInames.append(POIname)
+    listLongitudes.append(fLong)
+    listLatitudes.append(fLat)
+    
+    coords.append([fLong, fLat])
+    
     print ("({0})POI: {1} --- Long = {2} / Lat = {3}".format(
                         i,
-                        placemark.find('name').text,
+                        POIname, #placemark.find('name').text,
                         fLong, 
                         fLat))
 
-# =============================================================================
-# i = 1
-# for placemark in soup.Placemark.find_next_siblings ():
-#     print (i)
-#     i += 1
-#     print (placemark.find('name').string)
-#     fLong, fLat, _ = placemark.find('coordinates').text.split(',')
-#     fLong = float(fLong)
-#     fLat = float (fLat)
-#     print ("Long = {0} / Lat = {1}".format(fLong, fLat))
-# =============================================================================
